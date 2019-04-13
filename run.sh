c0b4e86ab5f92accab63990d220526e01c65d81f
Mikhail Surin <surinmike@gmail.com>
2019-04-13 19:55:14 +0300

    initial

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RAM_SIZE=5120
+NVM_SIZE=2048
+MAX_SIZE=$(($RAM_SIZE+$NVM_SIZE))
+
+qemu-system-x86_64 \
+    -hda base.qcow2 \
+    -machine accel=kvm,nvdimm \
+    -cpu host -smp 10 \
+    -m $RAM_SIZE,slots=3,maxmem=${MAX_SIZE}m \
+    -object memory-backend-file,id=mem1,mem-path=nvdimm_backend,size=${NVM_SIZE}m,pmem=on \
+    -device nvdimm,id=nvdimm1,memdev=mem1 \
+    -net user,hostfwd=tcp:127.0.0.1:5000-:22 \
+    -net nic \
+    -boot d \
+    --daemonize
