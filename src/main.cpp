8846d32e95e7282871f0ba17e58a846cca33306a
Mikhail Surin <surinmike@gmail.com>
2019-05-23 14:40:33 +0300

    sync

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,9 @@
 
 
 int main() {
-    printf("Hello\n");
+    PMEMobjpool* pool = pmemobj_create("/dev/pmem0", "layout", PMEMOBJ_MIN_POOL, 0666);
+    //PMEMobjpool* pool = pmemobj_open("/dev/pmem0", "layout");
+    if (pool == nullptr) {
+        return 1;
+    }
 }
